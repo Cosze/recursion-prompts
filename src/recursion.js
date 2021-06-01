f7f2d170163a8cf8d933c052173d2127e682c4b2
Cody Szeto <codyszet@gmail.com>
2021-05-31 20:19:24 -0700

    Implemented array sum function

--- src/recursion.js
+++ src/recursion.js
@@ -30,6 +30,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  if (!Array.isArray(array)) {
+    sum += array;
+    return sum;
+  }
+  array.forEach(function(number) {
+    sum += arraySum(number);
+  });
+  return sum;
 };
 
 // 4. Check if a number is even.
